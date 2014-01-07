9b6e41a25eb8e97294850e7b0ac7294c3d9e8e96
Michael Pickens <khariq@gmail.com>
2014-01-06 20:18:59 -0500

    Game state and resources

---
+++ Simulation/headers/GameState.h
@@ -0,0 +1,18 @@
+#ifndef Survive_Simulation_GameState_h
+#define Survive_Simulation_GameState_h
+
+#include "Resources.h"
+
+namespace Survive {
+	namespace Simulation {
+
+		class GameState
+		{
+		public:
+		private:
+			Resource::
+		};
+	}
+}
+
+#endif;
---
+++ Simulation/headers/Resources.h
@@ -0,0 +1,20 @@
+
+#ifndef Survive_Simulation_Resource_h
+#define Survive_Simulation_Resource_h
+
+
+namespace Survive {
+	namespace Simulation {
+		namespace Resource {
+			enum Resource
+			{
+				Food,
+				Wood,
+				Tools,
+				Oil
+			}
+		}
+	}
+}
+
+#endif
